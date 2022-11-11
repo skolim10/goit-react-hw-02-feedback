de54d62c8d83e8abdec16c31128624ebb2377ff1
Tomasz Skolimo <skolim10@gmail.com>
2022-11-11 19:39:35 +0100

    Section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Section } from './Section/Section';
+
+export class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Section title="Please leave feedback">
+          {/* <FeedbackOptions
+          options={['Good', 'Neutral', 'Bad']}
+          onLeaveFeedback={this.handleFeedback}
+        />{' '} */}
+        </Section>
+      </div>
+    );
+  }
+}
